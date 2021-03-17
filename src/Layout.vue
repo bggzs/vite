8aed63af12924db68c484fed3761561412afa545
bgg <1214246800@qq.com>
2021-03-17 16:08:53 +0800

    layout构建

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>HelloWorld</div>
+  <div class="layout">
+    <el-container>
+      <el-header></el-header>
+      <el-container>
+        <el-aside></el-aside>
+        <el-main> </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -14,4 +22,10 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.layout {
+  height: 100vh;
+  .el-aside {
+    height: calc(100vh - 60px);
+  }
+}
 </style>
